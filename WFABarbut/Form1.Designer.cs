02194a9675f344b26fb7aabc981664adf6b3b6e6
gzderdgan <erdoganngozde@gmail.com>
2019-03-24 22:43:28 +0300

    Initial commit

---
+++ WFABarbut/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WFABarbut
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.tmr1 = new System.Windows.Forms.Timer(this.components);
+            this.tmr2 = new System.Windows.Forms.Timer(this.components);
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb1
+            // 
+            this.pb1.Location = new System.Drawing.Point(120, 28);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(121, 137);
+            this.pb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb1.TabIndex = 0;
+            this.pb1.TabStop = false;
+            // 
+            // pb2
+            // 
+            this.pb2.Location = new System.Drawing.Point(337, 28);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(121, 137);
+            this.pb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb2.TabIndex = 0;
+            this.pb2.TabStop = false;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(120, 199);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(121, 23);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "Oyuncu 1 Zar AT";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Enabled = false;
+            this.btn2.Location = new System.Drawing.Point(337, 199);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(121, 23);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "Oyuncu 2 Zar AT";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(117, 269);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(341, 45);
+            this.lblSonuc.TabIndex = 2;
+            // 
+            // tmr1
+            // 
+            this.tmr1.Interval = 5;
+            this.tmr1.Tick += new System.EventHandler(this.tmr1_Tick);
+            // 
+            // tmr2
+            // 
+            this.tmr2.Interval = 5;
+            this.tmr2.Tick += new System.EventHandler(this.tmr2_Tick);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(585, 28);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(0, 13);
+            this.lbl1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(588, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(588, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.pb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.Timer tmr1;
+        private System.Windows.Forms.Timer tmr2;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WFABarbut/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFABarbut
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+        int sure = 10;
+        int sure2 = 10;
+        int x, y;
+        int s,t;
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            tmr2.Start();
+           
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            tmr1.Start();
+        }
+
+        private void tmr2_Tick(object sender, EventArgs e)
+        {
+           sure--;
+            x = rnd.Next(1, 7);
+            String bir = x + ".png";
+            pb1.Image = Image.FromFile(bir);
+
+            t = Convert.ToInt32(label1.Text);
+
+            if (sure == 0)
+            {
+                int sonuc = x + t;
+                label1.Text = sonuc.ToString();
+                tmr2.Stop();
+                sure = 10;
+                btn1.Enabled = false;
+                btn2.Enabled = true;
+
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tmr1_Tick(object sender, EventArgs e)
+        {
+            sure2--;
+            y = rnd.Next(1, 7);
+            String bir = y + ".png";
+            pb2.Image = Image.FromFile(bir);
+
+           
+            if (sure2 == 0)
+            {
+                int sonuc = s + y;
+                label2.Text = sonuc.ToString();
+                tmr1.Stop();
+                sure2 = 10;
+                btn1.Enabled = true;
+                btn2.Enabled = false;
+
+          }
+            s = Convert.ToInt32(label2.Text);
+            
+            if (x > y)
+                lblSonuc.Text = "Biirinci kazandı";
+            else if (x == y)
+            {
+                lblSonuc.Text = "Berabere";
+            }
+            else
+                lblSonuc.Text = "İkinci kazandı";
+
+        }
+    }
+}
